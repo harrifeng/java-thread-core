9f2d39982d506943640e59d68738984cf6748086
Harri Feng <harrifeng@gmail.com>
2018-11-06 16:33:21 +0800

    new example

--- src/main/java/org/hfeng/ch01/example03/WelcomeApp1.java
+++ src/main/java/org/hfeng/ch01/example03/WelcomeApp1.java
@@ -3,7 +3,6 @@ package org.hfeng.ch01.example03;
 public class WelcomeApp1 {
     public static void main(String[] args) {
         Thread welcomeThread = new Thread(new WelcomeTask());
-
         welcomeThread.start();
         System.out.printf("1.Welcome! I'm %s.%n", Thread.currentThread().getName());
     }
