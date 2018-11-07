e392dc79489fa9065f651a43aaaba02f7ac2d5ed
Harri Feng <harrifeng@gmail.com>
2018-11-07 17:17:09 +0800

    New example

---
+++ src/main/java/org/hfeng/misc/example01/DemoJoin.java
@@ -0,0 +1,25 @@
+package org.hfeng.misc.example01;
+
+import org.hfeng.util.Tools;
+
+public class DemoJoin {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new DemoThread());
+        thread1.start();
+
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Main thread is finished");
+    }
+}
+
+class DemoThread implements Runnable {
+    @Override
+    public void run() {
+        Tools.randomPause(1000);
+        System.out.println("Demo Thread Done");
+    }
+}
